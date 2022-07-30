385982bb11fa44757edba9b18fa3aa94cde78b11
Jared Daniel <jareddaniel95@gmail.com>
2022-07-30 14:36:09 -0400

    Add inserts for role and employee

--- index.js
+++ index.js
@@ -21,7 +21,48 @@ const newDeptOptions = [
         name: 'name',
         message: "Enter department name: "
     }
-]
+];
+
+const newRoleOptions = [
+    {
+        type: 'input',
+        name: 'dept',
+        message: "Enter department id: "
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: "Enter role title: "
+    },
+    {
+        type: 'input',
+        name: 'salary',
+        message: "Enter role salary: "
+    }
+];
+
+const newEmployeeOptions = [
+    {
+        type: 'input',
+        name: 'role',
+        message: "Enter role id: "
+    },
+    {
+        type: 'input',
+        name: 'first_name',
+        message: "Enter first name: "
+    },
+    {
+        type: 'input',
+        name: 'last_name',
+        message: "Enter last name: "
+    },
+    {
+        type: 'input',
+        name: 'manager',
+        message: "Enter manager id (leave blank for none): "
+    }
+];
 
 const db = mysql.createConnection(
     {
@@ -39,23 +80,55 @@ async function showDepartments() {
     });
 }
 
+async function showRoles() {
+    db.query('SELECT * FROM role', function(err, results) {
+        console.table(results);
+    });
+}
+
+async function showEmployees() {
+    db.query('SELECT * FROM employee', function(err, results) {
+        console.table(results);
+    });
+}
+
 async function action(answers) {
+    console.log();
     if(answers.choice == "view all departments") {
         await showDepartments();
     } else if(answers.choice == "view all roles") {
-        db.query('SELECT * FROM role', function(err, results) {
-            console.table(results);
-        });
+        showRoles();
     } else if(answers.choice == "view all employees") {
-        db.query('SELECT * FROM employee', function(err, results) {
-            console.table(results);
-        });
+        showEmployees();
     } else if(answers.choice == "add a department") {
         await inquirer
             .prompt(newDeptOptions)
             .then(async (answers) => {
                 db.query(`INSERT INTO department (d_name) VALUES("${answers.name}")`, async function(err, results) {
-                    await showDepartments();
+                    err ? console.log("Invalid entry") : await showDepartments();
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+        });
+    } else if(answers.choice == "add a role") {
+        await inquirer
+            .prompt(newRoleOptions)
+            .then(async (answers) => {
+                db.query(`INSERT INTO role (title, salary, department_id) VALUES("${answers.title}", ${answers.salary}, ${answers.dept})`, async function(err, results) {
+                    err ? console.log("Invalid entry") : await showRoles();
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+        });
+    } else if(answers.choice == "add an employee") {
+        await inquirer
+            .prompt(newEmployeeOptions)
+            .then(async (answers) => {
+                db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                            VALUES("${answers.first_name}", "${answers.last_name}", ${answers.role}, ${answers.manager})`, async function(err, results) {
+                    err ? console.log("Invalid entry\n" + err) : await showEmployees();
                 });
             })
             .catch((error) => {
@@ -64,7 +137,7 @@ async function action(answers) {
     } else if(answers.choice == "exit") {
         process.exit();
     }
-    await sleep(1000);
+    await sleep(500);
     prompt();
 }
 
